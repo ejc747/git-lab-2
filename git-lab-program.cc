4936eba21851be6d11882d5d0a9e95316014bc0d
ejc747 <ec169323@ohio.edu>
2024-02-04 14:41:42 -0500

    First Draft

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ethan Coyner
+ *        Date: 02/04/2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
